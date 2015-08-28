9d2904b801c6df36d0e7e07e012609b2c840970e
Grzegorz Walen <grzegorz.walen@altkominnovation.pl>
2015-08-28 16:06:42 +0200

    simple logging
    Change-Id: Ib66fb0a4d72fe366987cdab4e1721bd4593e8d3b

--- src/main/java/pl/codepot/dojrzewatr/api/BrewController.java
+++ src/main/java/pl/codepot/dojrzewatr/api/BrewController.java
@@ -1,5 +1,7 @@
 package pl.codepot.dojrzewatr.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -8,8 +10,14 @@ import pl.codepot.dojrzewatr.brewing.model.BrewRequest;
 @RestController
 @RequestMapping(value = "/brew", consumes = "application/vnd.pl.codepot.dojrzewatr.v1+json", produces = MediaType.APPLICATION_JSON_VALUE)
 public class BrewController {
+
+    private static final Logger logger = LoggerFactory.getLogger(BrewController.class);
+
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.OK)
     public void brew(@RequestBody BrewRequest brewRequest) {
+        logger.debug("Test hit blue dojrzewator");
     }
+
+
 }
