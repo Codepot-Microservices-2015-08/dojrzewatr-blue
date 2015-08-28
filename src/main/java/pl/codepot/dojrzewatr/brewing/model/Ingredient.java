636b5f7ed3957b8bb788cc54df3484911c077728
Ivan Vaskevych <ivan.vaskevych@allegrogroup.com>
2015-08-28 15:10:48 +0200

    Small styling

--- src/main/java/pl/codepot/dojrzewatr/brewing/model/Ingredient.java
+++ src/main/java/pl/codepot/dojrzewatr/brewing/model/Ingredient.java
@@ -1,54 +1,24 @@
 package pl.codepot.dojrzewatr.brewing.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class Ingredient {
 
-    @JsonProperty("type")
     private String type;
-    @JsonProperty("quantity")
     private Integer quantity;
 
 
-    /**
-     *
-     * @return
-     * The type
-     */
-    @JsonProperty("type")
     public String getType() {
         return type;
     }
 
-    /**
-     *
-     * @param type
-     * The type
-     */
-    @JsonProperty("type")
     public void setType(String type) {
         this.type = type;
     }
 
-    /**
-     *
-     * @return
-     * The quantity
-     */
-    @JsonProperty("quantity")
     public Integer getQuantity() {
         return quantity;
     }
 
-    /**
-     *
-     * @param quantity
-     * The quantity
-     */
-    @JsonProperty("quantity")
     public void setQuantity(Integer quantity) {
         this.quantity = quantity;
     }
-
-
 }
